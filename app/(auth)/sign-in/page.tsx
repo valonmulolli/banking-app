fc53f87b16733daf2cd7d6045aea70d8ab76a5be
valonmulolli <valonmulolli@gmail.com>
2024-05-03 00:00:44 +0200

    setup

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,5 @@
+function page() {
+	return <div>Sign-In</div>;
+}
+
+export default page;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+     SignUp 
+    </div>
+  )
+}
+
+export default page
