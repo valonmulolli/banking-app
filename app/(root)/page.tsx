e6087afca6f9f82f4a0c17df6453db1f4df859f3
valonmulolli <valonmulolli@gmail.com>
2024-05-03 18:12:38 +0200

    implement DoughnutChart component with static data for bank accounts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,28 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 const Home = () => {
-	return <div>Home</div>;
+  const loggedIn = { firstName: 'Valon' }
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome, "
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficently"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.23}
+          />
+        </header>
+      </div>
+    </section>
+  )
 };
 
-export default Home;
+export default Home
