ea4a2b92263379d1494e11fbac19ff4e941d4ae0
valonmulolli <valonmulolli@gmail.com>
2024-05-04 01:47:38 +0200

    implement responsive mobile navigation in Navbar and Sidebar components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,32 @@
 import type { Metadata } from 'next';
-import { IBM_Plex_Serif, Inter } from 'next/font/google';
+import {  IBM_Plex_Serif, Inter } from 'next/font/google';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 const ibmPlexSerif = IBM_Plex_Serif({
-	subsets: ['latin'],
-	weight: ['400', '700'],
-	variable: '--font-ibm-plex-serif',
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-ibm-plex-serif',
 });
 
 export const metadata: Metadata = {
-	title: 'Horizon Bank',
-	description: 'Horizon is modern banking platform for everyone',
-	icons: {
-		icon: '/icons/logo.svg',
-	},
+  title: 'Horizon Bank',
+  description: 'Horizon is modern banking platform for everyone',
+  icons: {
+    icon: '/icons/logo.svg',
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang='en'>
-			<body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang='en'>
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
 }
